2056a0fec880f157fe255eb77c5832629b551137
Aslam Parvej <aslamparvej2@gmail.com>
2023-01-15 00:20:37 +0530

    Modified footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>Copy right &#169; 2023 <span>Aslam Parvej</span></p>
+        <p>Copy right &#169; 2023 <span><a href="https://github.com/aslamparvej" target="_blank">Aslam Parvej</a></span></p>
         <p>Email: aslamparvej2@gmail.com</p>
     </div>
   )
